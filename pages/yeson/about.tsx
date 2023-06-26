e32b99b273493b5848dca02520e408917f94ae1f
yehyeon son <dara0615@ewhain.net>
2023-06-27 02:26:48 +0900

    [ feat ] : NextJs about.tsx practice

---
+++ pages/yeson/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const YesonAbout = () => {
+  return <St.Header>나는 예손🎈</St.Header>;
+};
+
+const St = {
+  Header: styled.div`
+    color: #832333;
+    font-size: 9rem;
+  `,
+};
+
+export default YesonAbout;
